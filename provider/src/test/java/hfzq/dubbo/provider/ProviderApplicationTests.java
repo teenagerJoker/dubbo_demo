fb07ae434f734d992be41bd50e5a8d0b516c9b88
teenagerJoker <ljw910910910@163.com>
2023-12-07 15:49:05 +0800

    更新

---
+++ provider/src/test/java/hfzq/dubbo/provider/ProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package hfzq.dubbo.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
